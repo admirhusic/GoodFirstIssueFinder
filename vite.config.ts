94297a4d5391d72cea8bf68f8f7d0a088c090142
Admir Husic <admirhusic91@gmail.com>
2024-07-03 19:24:38 +0200

    Added comment to vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,30 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import svgr from 'vite-plugin-svgr'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
 
 export default defineConfig({
-	// depending on your application, base can also be "/"
-	base: '',
-	plugins: [
-		svgr({
-			svgrOptions: { exportType: 'named', ref: true, svgo: false, titleProp: true },
-			include: '**/*.svg',
-		}),
-		react()],
-	server: {
-		// this ensures that the browser opens upon server start
-		open: true,
-		// this sets a default port to 3000  
-		port: 3000,
-	},
-})
+  // depending on your application, base can also be "/"
+  base: "",
+  plugins: [
+    /**
+     * This should probably be removed in future when we replace all svg icons.
+     *  Currently LoadingIndicator is left only.
+     * */
+    svgr({
+      svgrOptions: {
+        exportType: "named",
+        ref: true,
+        svgo: false,
+        titleProp: true,
+      },
+      include: "**/*.svg",
+    }),
+    react(),
+  ],
+  server: {
+    // this ensures that the browser opens upon server start
+    open: true,
+    // this sets a default port to 3000
+    port: 3000,
+  },
+});
