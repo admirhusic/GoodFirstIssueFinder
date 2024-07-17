2fe948e6cd957a5739846c7339dd9423f30225c1
kofta999 <mostafaxxx555@gmail.com>
2024-07-17 12:27:13 +0300

    Add language search

--- src/components/LanguageFilter.tsx
+++ src/components/LanguageFilter.tsx
@@ -1,6 +1,12 @@
 import React, { useState } from "react";
 import { strings } from "../strings";
 
+// TODO: The plan
+// Firstly I need to add more languages (dynamically if i could)
+// Then be able to search through them
+// When picking a language it should have like a small component with a small x button to delete the suggestion
+// then query GitHub after selecting those languages
+
 export interface LanguageFilterI {
   onLanguageChange: (language: string) => void;
 }
@@ -21,30 +27,36 @@ export default function LanguageFilter(props: LanguageFilterI) {
     "Angular",
     "Python",
   ].sort();
-  const [selectedLang, setSelectedLang] = useState("");
+  // const [selectedLang, setSelectedLang] = useState("");
+  const [searchQuery, setSearchQuery] = useState("")
 
-  const handleLangSelect = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    setSelectedLang(e.target.value);
-    props.onLanguageChange(e.target.value);
+
+  const filteredLanguages = languages.filter((lang) => lang.toLowerCase().includes(searchQuery.toLowerCase()))
+
+  const handleLangSelect = (lang: string) => {
+    // setSelectedLang(e.target.value);
+    props.onLanguageChange(lang);
   };
 
+  const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchQuery(e.target.value)
+  };
+
+  // TODO: STYLE NEEDED
   return (
     <div className="sm:w-full md:w-1/2 lg:w-1/2 mx-auto mb-3 flex flex-row items-start justify-start">
-      <select
-        className="shadow appearance-none border border-blue-950 rounded text-gray-700 leading-tight focus:outline-none focus:shadow-outline py-2 px-3"
-        value={selectedLang}
-        onChange={handleLangSelect}
-      >
-        <option disabled value="">
-          {strings.languageSelection}
-        </option>
-        <option value="">{strings.allLanguages}</option>
-        {languages.map((lang, key) => (
-          <option value={lang} key={key}>
-            {lang}
-          </option>
-        ))}
-      </select>
+
+      <input type="text" name="language" id="language" value={searchQuery} onChange={handleSearchChange} />
+
+      {searchQuery && (
+        <div className="">
+          {filteredLanguages.map(lang => (
+            <div key={lang} onClick={() => handleLangSelect(lang)}>
+              {lang}
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
