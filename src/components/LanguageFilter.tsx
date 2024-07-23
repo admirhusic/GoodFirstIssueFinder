e8ae6cd8ceade4af0a6aed8523263f478d3b61c3
kofta999 <mostafaxxx555@gmail.com>
2024-07-23 15:48:47 +0300

    Updated languages list

--- src/components/LanguageFilter.tsx
+++ src/components/LanguageFilter.tsx
@@ -1,3 +1,4 @@
+import { programmingLanguages } from "../constants";
 import { strings } from "../strings";
 import Select, { MultiValue } from "react-select";
 
@@ -5,31 +6,8 @@ export interface LanguageFilterI {
   onLanguageChange: (languages: string[]) => void;
 }
 
-interface ProgrammingLanguage {
-  value: string,
-  label: string
-}
-
 export default function LanguageFilter(props: LanguageFilterI) {
-  // TODO: Increase the list of languages
-  const languagesOptions =
-    [
-      { value: 'angular', label: 'Angular' },
-      { value: 'c', label: 'C' },
-      { value: 'c#', label: 'C#' },
-      { value: 'c++', label: 'C++' },
-      { value: 'codeigniter', label: 'Codeigniter' },
-      { value: 'java', label: 'Java' },
-      { value: 'javascript', label: 'Javascript' },
-      { value: 'kotlin', label: 'Kotlin' },
-      { value: 'laravel', label: 'Laravel' },
-      { value: 'php', label: 'PHP' },
-      { value: 'python', label: 'Python' },
-      { value: 'react', label: 'React' },
-      { value: 'vue', label: 'Vue' }
-    ]
-
-  const handleSearchChange = (languages: MultiValue<ProgrammingLanguage>) => {
+  const handleSearchChange = (languages: MultiValue<typeof programmingLanguages[number]>) => {
     const mappedValues = languages.map((lang) => lang.value)
     props.onLanguageChange(mappedValues)
   };
@@ -37,8 +15,7 @@ export default function LanguageFilter(props: LanguageFilterI) {
 
   return (
     <div className="sm:w-full md:w-1/2 lg:w-1/2 mx-auto mb-3 flex flex-col items-start justify-start">
-
-      <Select options={languagesOptions} isMulti placeholder={strings.searchLanguages} className="w-full" onChange={(langs) => handleSearchChange(langs)} />
+      <Select options={programmingLanguages} isMulti placeholder={strings.searchLanguages} className="w-full" onChange={(langs) => handleSearchChange(langs)} />
     </div>
   );
 }
